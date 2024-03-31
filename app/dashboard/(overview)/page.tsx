1328e9a0090f30e7c71d0a9b23792c099f5061da
kaitoito200198 <158575774+kaitoito200198@users.noreply.github.com>
2024-03-31 13:52:54 +0900

    chore:change  data.tsx

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,7 +4,7 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { inter } from '@/app/ui/fonts';
 import { fetchRevenue, fetchLatestInvoices,  fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton} from '@/app/ui/skeletons';
  
 export default async  function Page() {
     // const revenue = await fetchRevenue();
@@ -34,7 +34,9 @@ export default async  function Page() {
     <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-      <LatestInvoices latestInvoices={latestInvoices} />
+     <Suspense fallback={<LatestInvoicesSkeleton/>}>
+      <RevenueChart/>
+      </Suspense>   
     </div>
   </main>)
 }
